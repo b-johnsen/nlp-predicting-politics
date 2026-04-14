f9aff356fe8022543b3869fa5d12b3d38e481fd6
Jack Houck <jackahouck@gmail.com>
2026-04-14 15:37:18 -0400

    Added NER for executive orders (split)

---
+++ scripts/split_orders_ner.py
@@ -0,0 +1,35 @@
+import spacy 
+from pathlib import Path
+
+model = spacy.load("en_core_web_sm")
+
+# Uncomment to see list of labels
+# print(model.pipe_labels['ner'])
+
+def remove_entities(text, labels_to_remove={"PERSON", "GPE", "DATE", "ORG", "NORP"}):
+    ''' PERSON - names; GPE - states, countries, cities;
+        DATE - times; NORP - nationalities, religion/politics;
+        ORG - might remove govt agencies?'''
+    doc = model(text)
+    clean = text
+    for ent in reversed(doc.ents):
+        if ent.label_ in labels_to_remove:
+            clean = clean[:ent.start_char] + " " + clean[ent.end_char:]
+    return clean
+
+# This is if you are in the /scripts directory running the program
+input_dir = Path("../unclean_data/eo_labeled_split/")
+output_dir = Path("../clean_data/clean_eo_split/")
+
+for txt_file in input_dir.rglob("*.txt"):
+    text = txt_file.read_text(encoding="utf-8")
+    cleaned = remove_entities(text)
+    
+    # recreate the same folder structure in the output
+    out_path = output_dir / txt_file.relative_to(input_dir)
+    out_path.parent.mkdir(parents=True, exist_ok=True)
+    out_path.write_text(cleaned, encoding="utf-8")
+    
+    print(f"Cleaned: {txt_file}")
+
+print("Done!")
