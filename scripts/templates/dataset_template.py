f1a6f5cea0c17b5ce7b93bfc322c5c76f16c4d40
Jack E Timmermans <timmerja@bc.edu>
2026-04-14 16:54:51 -0400

    added template file for dataset and word2vec statistical model script

---
+++ scripts/templates/dataset_template.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+import os
+import pandas as pd
+
+def load_text_dataset(folder_0, folder_1):
+    data = []
+
+    def process_folder(root_folder, label):
+        for root, _, files in os.walk(root_folder):
+            for fname in files:
+                if fname.endswith(".txt"):
+                    path = os.path.join(root, fname)
+                    try:
+                        with open(path, 'r', encoding='utf-8', errors='ignore') as f:
+                            text = f.read()
+                        data.append({
+                            "text": text,
+                            "label": label,
+                            # "filepath": path # using to keep track of file?
+                        })
+                    except Exception as e:
+                        print(f"Skipping {path}: {e}")
+
+    # Process both folders
+    process_folder(folder_0, label=0)
+    process_folder(folder_1, label=1)
+
+    df = pd.DataFrame(data)
+
+    return df
+
+def main():
+  folder = Path("/Users/jacktimmermans/Documents/BC Spring 2026/NLP/nlp-predicting-politics/scripts/statistical modeling scripts/word2veceomodel")
+  os.makedirs(folder, exist_ok=True)
+
+  train_path = Path("nlp-predicting-politics/clean_data/clean_eo_split/train")
+
+  train_df = load_text_dataset(
+      train_path / "democrat",
+      train_path / "republican"
+  )
+
+  # run tokenization, etc on text column of dataframe and adjust as necessary
+  # see word2vecmodels for example
+
+  # labels
+  y_train = train_df["label"]
+
+  # saving dataframe
+  train_df.to_pickle(folder/"train_dataset.pkl")
+
+  test_path = Path("nlp-predicting-politics/clean_data/clean_eo_split/test")
+
+  test_df = load_text_dataset(
+      test_path / "democrat",
+      test_path / "republican"
+  )
+
+  # labels
+  y_test = test_df["label"]
+
+  # saving dataframe as compressed file for storage
+  test_df.to_pickle(folder/"test_dataset.pkl")
+
+
+if __name__ == "__main__":
+  main()
