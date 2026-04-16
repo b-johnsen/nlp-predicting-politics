24995780a344a7f761af6587a5aa1a5c03c41e76
Jack Houck <jackahouck@gmail.com>
2026-04-16 10:14:47 -0400

    Added pass so cnn.py doesnt error

--- deep_learning/cnn.py
+++ deep_learning/cnn.py
@@ -5,7 +5,7 @@ from torch.nn import functional as F
 
 class CNN(nn.module):
     def __init__(self, batch_size, output_size, in_channels, out_channels, kernel_heights, stride, padding, dropout_probab, vocab_size, embedding_length, weights):
-
+        pass
 
 
 
