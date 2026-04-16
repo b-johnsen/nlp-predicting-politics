e85d25dfcad28f6db8d7b0902fc5c6ec6c8a6ec1
Jack Houck <jackahouck@gmail.com>
2026-04-16 10:14:07 -0400

    Added deep learning folder for cnn and other models. Began writing cnn

---
+++ deep_learning/cnn.py
@@ -0,0 +1,13 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class CNN(nn.module):
+    def __init__(self, batch_size, output_size, in_channels, out_channels, kernel_heights, stride, padding, dropout_probab, vocab_size, embedding_length, weights):
+
+
+
+
+
+
